b666ecae2ae004caaf5c8a6a7458e8056a14df0f
Afca25 <afca25login@gmail.com>
2021-10-27 14:52:56 -0400

    git initiated

---
+++ public/script.js
@@ -0,0 +1,17 @@
+ 
+ 
+ // --------- Check geolocation (geolocation browser object) --------
+if ("geolocation" in navigator) {
+  /* geolocation is available */
+  console.log('Geolocation is available' + ' 🙂')
+  navigator.geolocation.getCurrentPosition(async position => {
+    console.log(position.coords.latitude, position.coords.longitude);
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
+    latPos.textContent = await latitude
+    longPos.textContent = await longitude
+  })
+} else {
+    /* geolocation is not available */
+    console.log('Geolocation is NOT available ' + ' 🙁')
+  }
