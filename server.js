9cf25dba1e78d6cdff941bb3590010544e0405a7
Afca25 <afca25login@gmail.com>
2021-10-27 15:55:46 -0400

    Database initialized using NeDB

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const express = require ('express')
+
 const app = express()
 app.listen(3000,() => console.log('Listening at 3000...'))
 app.use(express.static('public'))
 app.use(express.json({limit: '1mb'}))
+
+
+//? ---------- POST method -----------
+
+app.post('/api', (request, response) => {
+  console.log('I got a request')
+  const data = request.body
+
+  response.json(data)
+})
+
+//? ------------ GET method --------------
+
+app.get(('/api'), (request, response) => {
+  
+  response.json(data)
+})
